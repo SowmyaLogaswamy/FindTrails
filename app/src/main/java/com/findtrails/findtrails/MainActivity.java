c298f0ff30042fbddd0874477264c5dcc2a8cc3e
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-09-08 11:11:23 -0700

    Add a click listener to Explore button

--- app/src/main/java/com/findtrails/findtrails/MainActivity.java
+++ app/src/main/java/com/findtrails/findtrails/MainActivity.java
@@ -2,12 +2,25 @@ package com.findtrails.findtrails;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mExploreButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mExploreButton = (Button) findViewById(R.id.exploreButton);
+        mExploreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
+
