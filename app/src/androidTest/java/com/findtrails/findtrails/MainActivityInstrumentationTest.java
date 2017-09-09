e14acbb93689ab2beb2363726b0d6be81c51e4bb
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-09-08 17:05:50 -0700

    Add list view

---
+++ app/src/androidTest/java/com/findtrails/findtrails/MainActivityInstrumentationTest.java
@@ -0,0 +1,44 @@
+package com.findtrails.findtrails;
+
+/**
+ * Created by Guest on 9/8/17.
+ */
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
+                .check(matches(withText("Portland")));
+    }
+
+    @Test
+    public void locationIsSentToTrailsActivity() {
+        String location = "Portland";
+        onView(withId(R.id.locationEditText)).perform(typeText(location));
+        onView(withId(R.id.exploreButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches
+                (withText("We have found you trails near " + location)));
+    }
+
+
+
+}
--- app/src/main/java/com/findtrails/findtrails/TrailsActivity.java
+++ app/src/main/java/com/findtrails/findtrails/TrailsActivity.java
@@ -3,18 +3,50 @@ package com.findtrails.findtrails;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TrailsActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     private TextView mLocationTextView;
+    private ListView mListView;
+
+    private String[] trails = new String[] {"Discovery Park", "Pike Place Market",
+            "The Washington Park Arbore", "Green Lake Loop Trail", "Seattle Waterfront Pathway", "Seward Park",
+            "Mount Rosa Trail", "Burke Gilman Trail", "Alki Trail", "Foster Island Trail",
+            "Interurban Trail", "Camp Long Trails", "Columbine Trail",
+            "Tollantusky Trail", "Hensley Settlement"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trails);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, trails);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String trail = ((TextView)view).getText().toString();
+                Log.d(TAG, trail);
+                Toast.makeText(TrailsActivity.this, trail, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent =getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("We have found you trails near" + location);
+        mLocationTextView.setText("We have found you trails near " + location);
+
     }
 }
