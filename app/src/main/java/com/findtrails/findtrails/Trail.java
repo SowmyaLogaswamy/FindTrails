28b52cab20e27feb0eaed734b7e25f70dc2ec1d5
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-09-15 12:23:06 -0700

    Add a Trail model to store datax

---
+++ app/src/main/java/com/findtrails/findtrails/Trail.java
@@ -0,0 +1,41 @@
+package com.findtrails.findtrails;
+
+/**
+ * Created by Guest on 9/15/17.
+ */
+
+public class Trail {
+
+    private String mCity;
+    private String mState;
+    private String mName;
+    private String mDirections;
+
+
+    public Trail(String city, String state, String name, String directions) {
+
+        this.mCity = city;
+        this.mState = state;
+        this.mName = name;
+        this.mDirections = directions;
+
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public String getState() {
+        return mState;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getDirections() {
+        return mDirections;
+    }
+
+}
+
