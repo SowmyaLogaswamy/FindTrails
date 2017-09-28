1dec26f11eb78dd552ce68359e18d56301e37c83
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-09-28 16:55:26 -0700

    Add an activity to display the saved trails

---
+++ app/src/main/java/com/findtrails/findtrails/adapters/FirebaseTrailViewHolder.java
@@ -0,0 +1,74 @@
+package com.findtrails.findtrails.adapters;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.findtrails.findtrails.Constants;
+import com.findtrails.findtrails.R;
+import com.findtrails.findtrails.models.Trail;
+import com.findtrails.findtrails.ui.TrailsActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+public class FirebaseTrailViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+
+    View mView;
+    Context mContext;
+
+    public FirebaseTrailViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindRestaurant(Trail trail) {
+        TextView nameTextView = (TextView) mView.findViewById(R.id.trailNameTextView);
+        TextView descriptionTextView = (TextView) mView.findViewById(R.id.detailDescriptionTextView);
+        TextView directionTextView = (TextView) mView.findViewById(R.id.detailDirectionTextView);
+
+
+        nameTextView.setText(trail.getName());
+        descriptionTextView.setText("Description: " + trail.getDescription());
+        directionTextView.setText("Directions: " + trail.getDirections());
+    }
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Trail> trails = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_TRAILS);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    trails.add(snapshot.getValue(Trail.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, TrailsActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("trails", Parcels.wrap(trails));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/findtrails/findtrails/ui/SavedTrailListActivity.java
@@ -0,0 +1,59 @@
+package com.findtrails.findtrails.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.findtrails.findtrails.Constants;
+import com.findtrails.findtrails.R;
+import com.findtrails.findtrails.adapters.FirebaseTrailViewHolder;
+import com.findtrails.findtrails.models.Trail;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SavedTrailListActivity extends AppCompatActivity {
+    private DatabaseReference mTrailReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_trails);
+        ButterKnife.bind(this);
+
+        mTrailReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_TRAILS);
+        setUpFirebaseAdapter();
+    }
+
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Trail, FirebaseTrailViewHolder>
+                (Trail.class, R.layout.trails_list_item, FirebaseTrailViewHolder.class,
+                        mTrailReference) {
+
+            @Override
+            protected void populateViewHolder(FirebaseTrailViewHolder viewHolder,
+                                              Trail model, int position) {
+                viewHolder.bindTrail(model);
+            }
+        };
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
+}
